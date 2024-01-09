16c729891594c7657827f5f2bd0239285e0c411e
Caleb O'Malley <calebomalley@ufl.edu>
2024-01-09 00:03:29 -0500

    Exploring functions

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,35 @@
-//--------//
-// arrays //
-//--------//
+//-----------//
+// functions //
+//-----------//
 
-let animes: string[] = ['demon slayer', 'jjk', 'hxh']
-let outOf10: number[] = [7, 9, 10]
-
-// updating arrays
-animes.push('cowboy bebop')
-outOf10.push(10)
-
-// type inference with arrays
-let characters = ['gon', 'tanjiro', 'yuji itadori']
-characters.push('spike spiegel')
-const bestProtag = characters[3]
-
-// union type array
-let things = [1, true, 'hello', null]
-
-//-----------------//
-// object literals //
-//-----------------//
-
-let manga: {
-  title: string,
-  author: string,
-  chapter: number,
-  volume: number
-} = {
-  title: 'Uzumaki',
-  author: 'Junji Ito',
-  chapter: 1,
-  volume: 1
+function add(a: number, b: number): number {
+  return a + b
 }
 
-manga.chapter = 2
-
-//-------------------------------------//
-// type inference with object literals //
-//-------------------------------------//
-
-let originStory = {
-  parentsDead: true,
-  antagonistName: 'Joe Mama',
-  complexity: 'low'
+// arrow function syntax
+const subtract = (a: number, b: number): number => {
+  return a - b
 }
 
-originStory.complexity = 'high'
+const a = 60
+const b = 9
+console.log(`Add: ${a} + ${b} = ${add(a, b)}`)
+console.log(`Subtract: ${a} - ${b} = ${subtract(a, b)}`)
+
+// void return types
+function addAllNumbers(items: number[]): void {
+  const total = items.reduce((a, b) => a + b, 0)
+  console.log(total)
+}
+
+addAllNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+//-----------------------//
+// return type inference //
+//-----------------------//
+
+function formatGreeting(name: string, greeting: string) {
+  return `${greeting}, ${name}!`
+}
+
+console.log(formatGreeting('leonard', 'top of the morning'))
