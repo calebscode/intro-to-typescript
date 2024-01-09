b0a0fa7e48f7e110d15c7c8d56a37438560117d5
Caleb O'Malley <calebomalley@ufl.edu>
2024-01-08 23:49:36 -0500

    Exploring arrays

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,48 @@
-// explicit type assignment
-let age: number = 30
-let firstName: string = 'mario'
-let isFictional: boolean
+//--------//
+// arrays //
+//--------//
 
-// reassigning within a type
-age = 31
-firstName = 'luigi'
-isFictional = true
+let animes: string[] = ['demon slayer', 'jjk', 'hxh']
+let outOf10: number[] = [7, 9, 10]
 
-// inferred types
-let planet = 'Earth'
-let moons = 1
-let isLarge = false
+// updating arrays
+animes.push('cowboy bebop')
+outOf10.push(10)
 
-// null and undefined are also singleton types (probs not gonna use this ever)
-let nothing: null
-let alsoNothing: undefined
+// type inference with arrays
+let characters = ['gon', 'tanjiro', 'yuji itadori']
+characters.push('spike spiegel')
+const bestProtag = characters[3]
+
+// union type array
+let things = [1, true, 'hello', null]
+
+//-----------------//
+// object literals //
+//-----------------//
+
+let manga: {
+  title: string,
+  author: string,
+  chapter: number,
+  volume: number
+} = {
+  title: 'Uzumaki',
+  author: 'Junji Ito',
+  chapter: 1,
+  volume: 1
+}
+
+manga.chapter = 2
+
+//-------------------------------------//
+// type inference with object literals //
+//-------------------------------------//
+
+let originStory = {
+  parentsDead: true,
+  antagonistName: 'Joe Mama',
+  complexity: 'low'
+}
+
+originStory.complexity = 'high'
