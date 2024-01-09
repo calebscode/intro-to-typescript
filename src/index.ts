7fc2d1bba62da6844bf61d8647a78864c8931d2e
Caleb O'Malley <calebomalley@ufl.edu>
2024-01-08 23:29:03 -0500

    Basic type principles

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+// explicit type assignment
+let age: number = 30
+let firstName: string = 'mario'
+let isFictional: boolean
+
+// reassigning within a type
+age = 31
+firstName = 'luigi'
+isFictional = true
+
+// inferred types
+let planet = 'Earth'
+let moons = 1
+let isLarge = false
+
+// null and undefined are also singleton types (probs not gonna use this ever)
+let nothing: null
+let alsoNothing: undefined
